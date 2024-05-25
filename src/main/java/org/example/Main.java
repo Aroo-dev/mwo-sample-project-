2c9cbf0d10c38235ee5847b278369467550197c2
aroo-dev <axxrxxo@gmail.com>
2024-05-25 15:40:13 +0200

    implemented count average pit stops function

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,6 +2,7 @@ package org.example;
 
 import org.example.model.Race;
 import org.example.utils.JsonParser;
+import raceOperation.CountAvgPitStops;
 
 import java.io.IOException;
 import java.util.List;
@@ -15,6 +16,10 @@ public class Main {
 
             List<Race> races = JsonParser.parseRaceJson();
             races.forEach(System.out::println);
+            CountAvgPitStops avg = new CountAvgPitStops();
+            double v = avg.countAvgPitStops(races);
+            System.out.println(v);
+
 
         } catch (IOException e) {
             throw new RuntimeException(e);
